202b2bd83cfe5f86e478128cfd2ae63c028f399e
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-05-10 15:13:12 +0700

    masukkan class Stack

--- stackusinglinkedlist_0098/stackusinglinkedlist_0098.cpp
+++ stackusinglinkedlist_0098/stackusinglinkedlist_0098.cpp
@@ -4,8 +4,16 @@ using namespace std;
 //NOde class reprenting a single node in the linked list
 class Node {
 public:
+	int data;
+	Node* next;
 
-  
+	Node() {
+		next = NULL;
+	}
+};
+
+// Stack Class
+class Stack {
 
 };
 
