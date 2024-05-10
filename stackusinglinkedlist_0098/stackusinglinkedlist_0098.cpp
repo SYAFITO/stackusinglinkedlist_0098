c6f0804c78dafa81583aca3f4b1192e99c4abac0
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-05-10 15:43:32 +0700

    masukkan case 1

--- stackusinglinkedlist_0098/stackusinglinkedlist_0098.cpp
+++ stackusinglinkedlist_0098/stackusinglinkedlist_0098.cpp
@@ -64,13 +64,28 @@ public:
 };
 
 int main() {
-	Stack stock;
+	Stack stack;
 
 	int choice = 0;
 	int value;
 
 	while (choice != 5) {
+		cout << "1. Push\n";
+		cout << "2. Pop\n";
+		cout << "3. Peek\n";
+		cout << "4. Exit\n";
+		cout << " Enter Your Choice: ";
+		cin >> choice;
 
+		switch (choice) {
+		case 1:
+			cout << "Enter the value to push: ";
+			cin >> value;
+			stack.push(value);
+			break;
+			
+
+		}
 	}
 
 
