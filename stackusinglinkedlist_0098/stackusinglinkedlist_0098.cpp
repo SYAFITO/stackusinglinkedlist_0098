647d82ba71619b1c2da61b2a2e0da9bdb06eb5f8
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-05-10 15:16:54 +0700

    masukkan int push

--- stackusinglinkedlist_0098/stackusinglinkedlist_0098.cpp
+++ stackusinglinkedlist_0098/stackusinglinkedlist_0098.cpp
@@ -18,6 +18,15 @@ private:
 	Node* top; // Pointer to the top node of the stack
 
 public:
+	Stack() {
+		top = NULL; // Initialize the stack with a null top pointer
+	}
+
+	// Push operetion: Insert an element onto the top of the stack
+	int push(int value) {
+
+	}
+
 };
 
 
