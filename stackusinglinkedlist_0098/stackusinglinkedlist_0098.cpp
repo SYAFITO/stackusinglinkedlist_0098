abc8f2df3ea04ad949687aaea4b4e4a6d10f4524
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-05-10 15:47:15 +0700

    masukkan case 3

--- stackusinglinkedlist_0098/stackusinglinkedlist_0098.cpp
+++ stackusinglinkedlist_0098/stackusinglinkedlist_0098.cpp
@@ -90,7 +90,14 @@ int main() {
 			else {
 				cout << "Stack is Empty. Cannot pop." << endl;
 			}
-			
+			break;
+		case 3:
+			if (!stack.isEmpty()) {
+				stack.peek();
+			}
+			else {
+				cout << "Stack is Empty. No top value." << endl;
+			}
 
 		}
 	}
