8fd8dbb0eefb85a288aa80ce4f4569b2ba8d17f1
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-05-13 14:45:07 +0700

    masukkan class mahasiswa

---
+++ mbrStatistik.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+class mahasiswa
+{
+public:
+    static int nim;
+    int id;
+    string nama;
+
+void setID();
+
+void printALL();
+mahasiswa(string pnama):nama(pnama)
+{
+    setID;
+}
+
+};
+
+int mahasiswa::nim = 20;
+
+void mahasiswa::setID()
+{
+    id = nim++; 
+}
